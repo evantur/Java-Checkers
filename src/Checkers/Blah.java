4dd1f650172a25b50aaee074c26b8a44d024d9fd
Evan <eboyct@gmail.com>
2025-04-02 11:22:33 -0700

    Update Blah.java

--- src/Checkers/Blah.java
+++ src/Checkers/Blah.java
@@ -14,22 +14,15 @@ import java.util.Scanner;
 class Blah {
 
     public static void main (String [] args){
-
         Scanner nameScanner = (new Scanner("Whats your name?"));
         String anotherName = (nameScanner.nextLine());
 
-
         System.out.println(product(3, 4));
-
-
     }
 
     public static int product (int a, int b){
-
         int product = a*b;
         return product;
-
-
     }
 
 }
